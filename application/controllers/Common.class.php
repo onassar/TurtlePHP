47e1fe5927c0f685e5b808e4befb5999b5005923
Oliver Nassar <onassar@gmail.com>
2012-10-22 20:35:50 -0300

    Update application/controllers/Common.class.php
    Removed error action from common as it is now available controller-wide through the Controller class.

--- application/controllers/Common.class.php
+++ application/controllers/Common.class.php
@@ -11,32 +11,6 @@
     final class CommonController extends \Turtle\Controller
     {
         /**
-         * error
-         *
-         * 404 requests that come in.
-         * 
-         * @access public
-         * @param  string $path
-         * @return void
-         */
-        public function error($path)
-        {
-            // agent storage (check done with respect to spiders)
-            $agent = isset($_SERVER['HTTP_USER_AGENT'])
-                ? $_SERVER['HTTP_USER_AGENT']
-                : '(unknown)';
-
-            // log
-            error_log(
-                "\n" .
-                "**Invalid Request**\n" .
-                "Path: *" . ($path) . "*\n" .
-                "Remote Address: *" . (IP) . "*\n" .
-                "Agent: *" . ($agent) ."*\n"
-            );
-        }
-
-        /**
          * index
          * 
          * @access public
