13d529b301cf3dcde6982cab6e4853a9a4ed1115
Oliver Nassar <onassar@gmail.com>
2013-02-10 17:14:37 -0500

    Update application/views/common/404.inc.php

--- application/views/common/404.inc.php
+++ application/views/common/404.inc.php
@@ -13,7 +13,6 @@
     <head>
         <meta charset="utf-8" />
         <title>Whoops.</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="" />
         <meta name="author" content="" />
 
