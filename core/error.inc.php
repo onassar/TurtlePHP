47adf97714879f1b0828f38f747a2b7423f95147
Oliver Nassar <onassar@gmail.com>
2013-02-10 17:14:19 -0500

    Update core/error.inc.php

--- core/error.inc.php
+++ core/error.inc.php
@@ -14,7 +14,6 @@
     <head>
         <meta charset="utf-8" />
         <title>Whoops.</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="" />
         <meta name="author" content="" />
 
