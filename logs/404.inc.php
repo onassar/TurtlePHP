f02a39a1389b87b012bd9532c8a15a4d9336b16c
Oliver Nassar <onassar@gmail.com>
2020-08-14 22:34:11 -0400

    Delete 404.inc.php

--- logs/404.inc.php
+++
@@ -1,8 +0,0 @@
-
-*Invalid Request*
-*Host*              : <?= ($_SERVER['HTTP_HOST'] ?? ':unknown:') . (PHP_EOL) ?>
-*Request Path*      : <?= TurtlePHP\Application::getRequest()->getPath() . (PHP_EOL) ?>
-*Request URI*       : <?= ($_SERVER['REQUEST_URI'] ?? ':unknown:') . (PHP_EOL) ?>
-*Referrer*          : <?= ($_SERVER['HTTP_REFERER'] ?? ':unknown:') . (PHP_EOL) ?>
-*IP*                : <?= (IP) . (PHP_EOL) ?>
-*Agent*             : <?= ($_SERVER['HTTP_USER_AGENT'] ?? ':unknown:') . (PHP_EOL) ?>
